892ae7a08001fe369242b5a0b3acb33796a5a535
Jaitesh Godara <gjaitesh@gmail.com>
2024-03-18 12:42:25 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/todos'
+
 
 function App() {
 
 
   return (
     <>
-     <h1>This is Redux learning</h1>
+    <h1 className="text-3xl font-bold underline">This is Redux learning</h1>
+    <AddTodo/>
+    <Todos/>
     </>
   )
 }
